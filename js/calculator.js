15cb4eca5c73571024b7fb6212c82a57c7565de5
jpoe <16984557+jpoehl@users.noreply.github.com>
2022-06-17 21:23:40 +0200

    feat: Add function to display result

--- js/calculator.js
+++ js/calculator.js
@@ -40,3 +40,25 @@ function operate(a, b, o) {
 
     return result;
 }
+
+// Format for display
+/// Obtain the number of decimal places (https://stackoverflow.com/questions/9539513/is-there-a-reliable-way-in-javascript-to-obtain-the-number-of-decimal-places-of)
+function decimalPlaces(n) {
+    function hasFraction(n) {
+        return Math.abs(Math.round(n) - n) > 1e-10;
+    }
+
+    let count = 0;
+    // multiply by increasing powers of 10 until the fractional part is ~ 0
+    while (hasFraction(n * (10 ** count)) && isFinite(10 ** count))
+        count++;
+    return count;
+}
+
+function display(x, decimals = 10) {
+    if (decimalPlaces(x) > decimals) {
+        return x.toPrecision(decimals);
+    } else {
+        return x.toString();
+    }
+}
